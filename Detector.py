0de7761115e0a8528b51095ea4a37e206e3ca424
Tiziano2612 <TizianoRoig26@hotmail.com>
2024-11-01 14:09:16 -0300

    Nuevos Detectores

--- Detector.py
+++ Detector.py
@@ -4,50 +4,46 @@ class Detector:
         self.nombre_matriz = nombre_matriz
 
     def detectar_mutantes(self):
-        # Comprobación de mutantes horizontales
+        mutante = False
+        #mutantes horizontales
         for i, fila in enumerate(self.matriz):
             if self.hay_mutacion(fila):
-                #print(f'¡Mutación en la fila {i + 1}!')
-                return True
+                #print(f'Mutación en la fila {i + 1}')
+                mutante = True
 
-        # Comprobación de mutantes verticales
+        #mutantes verticales
         for j in range(len(self.matriz[0])):
             columna = [self.matriz[i][j] for i in range(len(self.matriz))]
             if self.hay_mutacion(columna):
-                #print(f'¡Mutación en la columna {j + 1} {columna}!')
-                return True
+                #print(f'Mutación en la columna {j + 1} {columna}')
+                mutante = True
 
-        # Comprobación de mutantes diagonales
-        diagonales = self.extraer_diagonales()
-        for d in diagonales:
+        #mutantes diagonales
+        diagonal1, diagonal2 = self.extraer_diagonales()
+        print("Diagonales: ")
+        print(diagonal1, " - ", diagonal2)
+        for d in diagonal1:
             if self.hay_mutacion(d):
-                #print(f'¡Mutación en una diagonal!')
-                return True
+                #print(f'Mutación en una diagonal')
+                mutante = True
+        for d in diagonal2:
+            if self.hay_mutacion(d):
+                #print(f'Mutación en una diagonal')
+                mutante = True
+        return mutante
 
     def hay_mutacion(self, secuencia):
         return secuencia.count('A') >= 4 or secuencia.count('T') >= 4 or secuencia.count('C') >= 4 or secuencia.count('G') >= 4  
 
     def extraer_diagonales(self):
-        diagonales = []
-        n = len(self.matriz)
-        # Diagonales de izquierda a derecha
-        for i in range(n):
-            diagonal = []
-            for j in range(n):
-                if i + j < n:
-                    diagonal.append(self.matriz[i + j][j])
-            if diagonal:
-                diagonales.append(diagonal)
-        # Diagonales de derecha a izquierda
-        for i in range(n):
-            diagonal = []
-            for j in range(n):
-                if i + j < n:
-                    diagonal.append(self.matriz[i + j][n - 1 - j])
-            if diagonal:
-                diagonales.append(diagonal)
-        return diagonales
-    
+        diagonal1 = ""
+        diagonal2 = ""
+        for i in range(len(self.matriz)):
+            diagonal1 += (self.matriz[i][i])
+        for i in range(len(self.matriz)):
+            diagonal2 += (self.matriz[i][len(self.matriz) - 1 - i])  
+        return diagonal1, diagonal2
+        
     
     
     
