82cbe2768f92445a7679fbdc60c03b4ebc94cb57
RussoLucas-Lab <lucasrusso_1999@hotmail.com>
2024-11-14 21:49:52 -0300

    Implemento detectar_mutacion_diagonal

--- Detector.py
+++ Detector.py
@@ -2,9 +2,14 @@ class Detector:
     def __init__(self, matriz, nombre_matriz):
         self.matriz = matriz
         self.nombre_matriz = nombre_matriz
+        
+        #añadi base_nitrogenada para almacenar la letra que se repite
         self.base_nitrogenada = ""
 
     def detectar_mutantes(self):
+        
+        # ACA llamar a 3 metodos privados (detectar_mutacion_horizontal, detectar_mutacion_vertical, detectar_mutacion_diagonal)
+        
         mutante = []
         j = 0
         Mutacion = []
@@ -29,12 +34,12 @@ class Detector:
 
     def hay_mutacion(self, secuencia):
         return secuencia.count('A') >= 4 or secuencia.count('T') >= 4 or secuencia.count('C') >= 4 or secuencia.count('G') >= 4
-            
     
-    def detectar_mutacion_diagonal(self):
+    # Método privado se encarga de analizar las diagonales de la matriz, debe ser llamado en detectar_mutantes
+    def __detectar_mutacion_diagonal__(self):
         filas = len(self.matriz)
         columnas = len(self.matriz[0])
-        base_nitrogenada = ""
+        
         # Diagonal de izquierda a derecha 
         for i in range(filas):
             for j in range(columnas):
@@ -50,16 +55,18 @@ class Detector:
                     if (self.matriz[i][j] == self.matriz[i + 1][j - 1] == self.matriz[i + 2][j - 2] == self.matriz[i + 3][j - 3]):
                         self.base_nitrogenada = self.matriz[i][j]
                         
-        
+        # Mensaje que se muestra en pantalla indicando que mutacion se encontró
+        # Retorno de base_nitrogenada, se retorna un caracter ('T' por ejemplo)
+
         if self.base_nitrogenada == 'A':
-    
-                return print(f'Mutación de Adenina en una diagonal')
+                return print(f'Mutación de Adenina en una diagonal'),self.base_nitrogenada
         elif self.base_nitrogenada == 'T':
-                return print(f'Mutación de Timina en una diagonal')
+                return print(f'Mutación de Timina en una diagonal'),self.base_nitrogenada
         elif self.base_nitrogenada == 'C':
-                return print(f'Mutación de Citosina en una diagonal')
+                return print(f'Mutación de Citosina en una diagonal'),self.base_nitrogenada
         elif self.base_nitrogenada == 'G':
-                return print(f'Mutación de Guanina en una diagonal')        
+                return print(f'Mutación de Guanina en una diagonal'),self.base_nitrogenada       
         else: print("ALgo anda mal")    
         return False
 
+    
--- PRUEBAS.py
+++
@@ -1,7 +0,0 @@
-import random
-from Detector import Detector 
-
-matriz_prueba = ["ATAsd","AATAjq","aAwTAA","AAAATA","AApaSA","AAAABBA"] 
-
-adn = Detector(matriz_prueba, "pepe")
-adn.detectar_mutacion_diagonal()
