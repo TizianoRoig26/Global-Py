79d806fa1dec2250f992e1abac62219a3016ce7f
TizianoRoig26 <142861158+TizianoRoig26@users.noreply.github.com>
2024-11-16 19:56:08 -0300

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -2,71 +2,96 @@ class Detector:
     def __init__(self, matriz, nombre_matriz):
         self.matriz = matriz
         self.nombre_matriz = nombre_matriz
-        
-        #añadi base_nitrogenada para almacenar la letra que se repite
-        self.base_nitrogenada = ""
 
     def detectar_mutantes(self):
-        
-        # ACA llamar a 3 metodos privados (detectar_mutacion_horizontal, detectar_mutacion_vertical, detectar_mutacion_diagonal)
-        
         mutante = []
-        j = 0
         Mutacion = []
-    
-        for i, fila in enumerate(self.matriz):
-            if self.hay_mutacion(fila):
-                #print(f'Mutación en la fila {i + 1}')
-                mutante.append('Horizontales')
-                Mutacion.insert(i + 1, fila)
+        base = []
 
-        #mutantes verticales
-        for j in range(len(self.matriz[0])):  # Iterar sobre las columnas
-            columna = [self.matriz[i][j] for i in range(len(self.matriz))]  # Obtener la columna
-            if self.hay_mutacion(columna):  # Comprobar si hay mutación en la columna
-                mutante.append('Verticales')
-                Mutacion.insert(j + 1, ''.join(columna))  # Usar 'C' para columna
+        # Verificar mutaciones diagonales
+        mutantes_diagonales, mutaciones_diagonales = self.__mutantes_diagonales()
+        mutante.extend(mutantes_diagonales)
+        Mutacion.extend(mutaciones_diagonales)
 
-        if mutante[0] != 0 and Mutacion[0] != 0:
-            return True, mutante, Mutacion
+        # Verificar mutaciones horizontales
+        mutantes_horizontales, mutaciones_horizontales = self.__mutantes_horizontales()
+        mutante.extend(mutantes_horizontales)
+        Mutacion.extend(mutaciones_horizontales)
+
+        # Verificar mutaciones verticales
+        mutantes_verticales, mutaciones_verticales = self.__mutantes_verticales()
+        mutante.extend(mutantes_verticales)
+        Mutacion.extend(mutaciones_verticales)
+
+        if mutante and Mutacion:
+            base = self.detectar_base_mutante(Mutacion)
+            return True, mutante, Mutacion, base
         else:
             return False
 
+    def __mutantes_horizontales(self):
+        mutante = []
+        Mutacion = []
+        for i, fila in enumerate(self.matriz):
+            if self.hay_mutacion(fila):
+                mutante.append(f'Horizontal en fila {i + 1}')
+                Mutacion.append(''.join(fila))
+        return mutante, Mutacion
+
+    def __mutantes_verticales(self):
+        mutante = []
+        Mutacion = []
+        for j in range(len(self.matriz[0])):  # Iterar sobre las columnas
+            columna = [self.matriz[i][j] for i in range(len(self.matriz))]  # Obtener la columna
+            if self.hay_mutacion(columna):  # Comprobar si hay mutación en la columna
+                mutante.append(f'Vertical en columna {j + 1}')
+                Mutacion.append(''.join(columna))
+        return mutante, Mutacion
+
+    def __mutantes_diagonales(self):
+        mutante = []
+        Mutacion = []
+        diagonales = self.extraer_diagonales()
+        for i, diagonal in enumerate(diagonales):
+            if self.hay_mutacion(diagonal):
+                mutante.append(f'Diagonal {i + 1}')
+                Mutacion.append(diagonal)
+        return mutante, Mutacion
+
     def hay_mutacion(self, secuencia):
         return secuencia.count('A') >= 4 or secuencia.count('T') >= 4 or secuencia.count('C') >= 4 or secuencia.count('G') >= 4
-    
-    # Método privado se encarga de analizar las diagonales de la matriz, debe ser llamado en detectar_mutantes
-    def __detectar_mutacion_diagonal__(self):
-        filas = len(self.matriz)
-        columnas = len(self.matriz[0])
-        
-        # Diagonal de izquierda a derecha 
-        for i in range(filas):
-            for j in range(columnas):
-                if i + 3 < filas and j + 3 < columnas:
-                    if (self.matriz[i][j] == self.matriz[i + 1][j + 1] == self.matriz[i + 2][j + 2] == self.matriz[i + 3][j + 3]):
-                        self.base_nitrogenada = self.matriz[i][j]
-                        
 
-        # Diagonal de derecha a izquierda
-        for i in range(filas):
-            for j in range(columnas):
-                if i + 3 < filas and j - 3 >= 0:  
-                    if (self.matriz[i][j] == self.matriz[i + 1][j - 1] == self.matriz[i + 2][j - 2] == self.matriz[i + 3][j - 3]):
-                        self.base_nitrogenada = self.matriz[i][j]
-                        
-        # Mensaje que se muestra en pantalla indicando que mutacion se encontró
-        # Retorno de base_nitrogenada, se retorna un caracter ('T' por ejemplo)
+    def extraer_diagonales(self):
+        diagonales = []
 
-        if self.base_nitrogenada == 'A':
-                return print(f'Mutación de Adenina en una diagonal'),self.base_nitrogenada
-        elif self.base_nitrogenada == 'T':
-                return print(f'Mutación de Timina en una diagonal'),self.base_nitrogenada
-        elif self.base_nitrogenada == 'C':
-                return print(f'Mutación de Citosina en una diagonal'),self.base_nitrogenada
-        elif self.base_nitrogenada == 'G':
-                return print(f'Mutación de Guanina en una diagonal'),self.base_nitrogenada       
-        else: print("ALgo anda mal")    
-        return False
+        # Diagonales de izquierda a derecha (\\)
+        for start in range(-5, 6):  # Rango para todas las diagonales posibles en una matriz 6x6
+            diagonal = []
+            for i in range(6):
+                j = i + start
+                if 0 <= j < 6:
+                    diagonal.append(self.matriz[i][j])
+            if len(diagonal) >= 4:
+                diagonales.append(''.join(diagonal))
+
+        # Diagonales de derecha a izquierda (/)
+        for start in range(11):  # Rango para todas las diagonales posibles en una matriz 6x6
+            diagonal = []
+            for i in range(6):
+                j = start - i
+                if 0 <= j < 6:
+                    diagonal.append(self.matriz[i][j])
+            if len(diagonal) >= 4:
+                diagonales.append(''.join(diagonal))
+
+        return diagonales
+
+    def detectar_base_mutante(self, mutaciones):
+        bases_mutantes = []
+        for mutacion in mutaciones:
+            conteo = {base: mutacion.count(base) for base in "ATCG"}
+            base_predominante = max(conteo, key=conteo.get)  # Base con mayor frecuencia
+            bases_mutantes.append(base_predominante)
+        return bases_mutantes
 
     
