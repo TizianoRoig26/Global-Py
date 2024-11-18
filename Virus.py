5fa32ac8a9d4aef54f14b85cb307a13c1e80661e
RussoLucas-Lab <lucasrusso_1999@hotmail.com>
2024-11-18 20:27:50 -0300

    Mutador, virus y Radiacion terminados

--- Virus.py
+++ Virus.py
@@ -1,20 +1,21 @@
 from Mutador import Mutador
+from Radiacion import Radiacion
 
-class Radiacion(Mutador):
-    
-    def __init__(self, matriz,base_nitrogenada ):
+class Virus(Mutador):
+
+    def __init__(self, matriz, base_nitrogenada):
         super().__init__(matriz, base_nitrogenada)
         self.coordenada_fila = 0
         self.coordenada_columna = 0
-        self.orientacion_muatcion = 0    
+        self.orientacion_muatcion = 0 
 
     def crear_mutante(self):
        
         # Validacion de la orientacion de la mutación
         while True:
             try:
-                print("Ingrese la orientación para la mutación")
-                self.orientacion_muatcion = int(input("(1) Vertical | (2) Horizontal: "))
+                print("Ingrese la orientación para la mutación diagonal")
+                self.orientacion_muatcion = int(input("(1) Izquierda a derecha | (2) Derecha a izquierda: "))
                 if self.orientacion_muatcion == 1 or  self.orientacion_muatcion == 2:
                     break  
                 else:
@@ -24,12 +25,11 @@ class Radiacion(Mutador):
 
         # Llamamos al método validacion_datos para solicitar al usuario que ingrese las coordenadas
         self.coordenada_fila, self.coordenada_columna = self.__validacion_datos()
-
-        # Según la orientacion, se llama a los métodos privados para realizar la mutación
+        
         if self.orientacion_muatcion == 1:
-            self.matriz = self.__mutador_vertical(self.coordenada_fila, self.coordenada_columna, self.base_nitrogenada)
+            self.matriz = self.__mutador_izquierda_derecha(self.coordenada_fila, self.coordenada_columna, self.base_nitrogenada)
         else:
-            self.matriz = self.__muatdor_horizontal(self.coordenada_fila, self.coordenada_columna, self.base_nitrogenada)
+            self.matriz = self.__mutador_derecha_izquierda(self.coordenada_fila, self.coordenada_columna, self.base_nitrogenada)
 
     def __validacion_datos(self):
 
@@ -37,7 +37,7 @@ class Radiacion(Mutador):
         while True:  
             try:
                 
-                if self.orientacion_muatcion == 1:
+                if self.orientacion_muatcion == 1: 
                     self.coordenada_fila = int(input("Ingrese la coordenada de la fila: " ))
                     if 0 <= self.coordenada_fila <= 2:
                         break  
@@ -45,10 +45,10 @@ class Radiacion(Mutador):
                         print("Error: El número debe estar entre 0 y 2.")
                 else:
                     self.coordenada_fila = int(input("Ingrese la coordenada de la fila: " ))
-                    if 0 <= self.coordenada_fila <= 5:
+                    if 0 <= self.coordenada_fila <= 2:
                         break  
                     else:
-                        print("Error: El número debe estar entre 0 y 5.")        
+                        print("Error: El número debe estar entre 0 y 2.")        
             except ValueError:
                 print("Error: Debes ingresar un número entero.")
 
@@ -57,51 +57,23 @@ class Radiacion(Mutador):
             try:
                 if self.orientacion_muatcion == 2:    
                     self.coordenada_columna = int(input("Ingrese la coordenada de la  columna: "))
+                    if 3 <= self.coordenada_columna <= 5:
+                        break  
+                    else:
+                        print("Error: El número debe estar entre 3 y 5.")
+                else:
+                    self.coordenada_columna = int(input("Ingrese la coordenada de la  columna: "))
                     if 0 <= self.coordenada_columna <= 2:
                         break  
                     else:
-                        print("Error: El número debe estar entre 0 y 2.")
-                else:
-                    self.coordenada_columna = int(input("Ingrese la coordenada de la  columna: "))
-                    if 0 <= self.coordenada_columna <= 5:
-                        break  
-                    else:
-                        print("Error: El número debe estar entre 0 y 5.")        
+                        print("Error: El número debe estar entre 0 y 2.")        
             except ValueError:
                 print("Error: Debes ingresar un número entero.")
 
 
-        return self.coordenada_fila, self.coordenada_columna
+        return self.coordenada_fila, self.coordenada_columna    
 
-    def __muatdor_horizontal(self,coordenada_fila, coordenada_columna, base_nitrogenada):
-        
-        lista_cadena =[]
-        # Separa los caracteres de cada fila y los une en una sola linea
-        for elemento in self.matriz:
-            for caracter in elemento:
-                lista_cadena.append(caracter)
-
-        base_nitrogenada_reemplazo =[]
-        # Bucle para creear la base nitrogenada que infecta el ADN
-        base_nitrogenada_reemplazo += ''.join(self.base_nitrogenada for _ in range(4))
-
-        # Utilizamos posicion_inicial como coordenada para reemplazar las bases nitrogenadas en la cadena desarmada
-        posicion_inicial = self.coordenada_fila * 6 + self.coordenada_columna
-        posicion_final = posicion_inicial + 4
-
-        # Reemplaza los caraacteres con los correspondientes al de la base nitrogenada
-        lista_cadena = lista_cadena[:posicion_inicial] + base_nitrogenada_reemplazo + lista_cadena[posicion_final:] # Con slicing reemplazamos los 4 caracteres de la lista con los 4 de la base nitrogenada
-
-        # vuelve a unir los caracteres en sus correspondientes cadenas
-        lista_cadena=[''.join(lista_cadena[i:i+6]) for i in range(0, len(lista_cadena), 6)] # Con el metodo join unimos los caraccteres, tomamos tramos de 6 caracteres y los agregamos a la lista
-
-        
-        for elemento in lista_cadena:
-            print("|",elemento,"|")
-
-        return lista_cadena
-
-    def __mutador_vertical(self,coordenada_fila, coordenada_columna, base_nitrogenada):
+    def __mutador_izquierda_derecha(self,coordenada_fila, coordenada_columna, base_nitrogenada):
         lista_cadena = []
     
         for elemento in self.matriz:
@@ -115,7 +87,31 @@ class Radiacion(Mutador):
         
         posicion_final = posicion_inicial + 1 
 
-        for i in range(0,24, 6):
+        for i in range(0,28, 7):
+            lista_cadena = lista_cadena[:posicion_inicial + i] + base_nitrogenada_reemplazo + lista_cadena[posicion_final + i :]
+
+        lista_cadena=[''.join(lista_cadena[i:i+6]) for i in range(0, len(lista_cadena), 6)]
+        
+        for elemento in lista_cadena:
+            print("|",elemento,"|")
+
+        return lista_cadena        
+
+    def __mutador_derecha_izquierda(self,coordenada_fila, coordenada_columna, base_nitrogenada):
+        lista_cadena = []
+    
+        for elemento in self.matriz:
+            for caracter in elemento:
+                lista_cadena.append(caracter)
+        
+        base_nitrogenada_reemplazo = []
+        base_nitrogenada_reemplazo = list(self.base_nitrogenada)
+
+        posicion_inicial = self.coordenada_fila * 6 + self.coordenada_columna
+        
+        posicion_final = posicion_inicial + 1 
+
+        for i in range(0,20, 5):
             lista_cadena = lista_cadena[:posicion_inicial + i] + base_nitrogenada_reemplazo + lista_cadena[posicion_final + i :]
 
         lista_cadena=[''.join(lista_cadena[i:i+6]) for i in range(0, len(lista_cadena), 6)]
--- prueba.py
+++ prueba.py
@@ -1,8 +1,11 @@
 from Mutador import Mutador
 from Radiacion import Radiacion
+from Virus import Virus
 
-adn = Radiacion(["123456","123456","123456","123456","123456","123456"], 'T')
+adn = Virus(["123456","123456","123456","123456","123456","123456"], 'T')
+adn_radiacion = Radiacion(["123456","123456","123456","123456","123456","123456"], 'T')
+#adn = Radiacion(["123456","123456","123456","123456","123456","123456"], 'T')
+adn_radiacion.crear_mutante()
 
-adn.crear_mutante()
-
-
+#adn_virus = Virus(["123456","123456","123456","123456","123456","123456"], 'T')
+#adn.crear_mutante()
